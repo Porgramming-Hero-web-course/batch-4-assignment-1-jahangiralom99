b6f7d5a2c690824d09d1782b07498c8fb529c266
Jahangir Alom <108364299+jahangiralom99@users.noreply.github.com>
2024-11-08 23:20:53 +0600

    Update problem-4.ts

--- problem-4.ts
+++ problem-4.ts
@@ -24,10 +24,10 @@ function calculateShapeArea(value: U): number {
   return 0;
 }
 
-const circleArea = calculateShapeArea({ shape: "circle", radius: 5 });
-const rectangleArea = calculateShapeArea({
-  shape: "rectangle",
-  width: 4,
-  height: 6,
-});
-console.log(circleArea, rectangleArea);
+// const circleArea = calculateShapeArea({ shape: "circle", radius: 5 });
+// const rectangleArea = calculateShapeArea({
+//   shape: "rectangle",
+//   width: 4,
+//   height: 6,
+// });
+// console.log(circleArea, rectangleArea);
